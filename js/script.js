c58fa00fb731af85f125d04e12b1117105fcb355
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro-2.local>
2012-01-27 17:30:15 -0800

    Adjusted calcs for bpm

--- js/script.js
+++ js/script.js
@@ -4,29 +4,29 @@
 
 $(function() {
     var $output = $('h1 span');
-    var seconds = 0;
-    var interval;
-    var milliseconds = 0;
-    var count1 = 0;
-    var count2 = 0;
+    var  date = new Date();
+    var beats = [{'time': date.getTime(), 'frequency': date.getTime()}];
+    var time;
     var bpm = 0;
     
-    interval = setInterval(function() {
-        milliseconds++;
-        /*
-if ($seconds >= 60) {
-            $seconds = 0;
-            $count = 0;
-        }
-        $seconds++;
-        $output.text();
-*/
-        $output.text(Math.round(bpm));
-    }, 1);
+    var interval;
     
+    /*
+interval = setInterval(function() {
+        $.each(beats, function() {
+            totalFrequency += this['frequency'];
+        });
+        //console.log(beats.length);
+        bpm = (60000*beats.length)/totalFrequency;
+        //console.log(totalFrequency);
+        console.log(bpm);
+        //$output.text(Math.round(bpm));
+    }, 1000); 
+*/ 
     
     $('body').bind('click', function() {
-        count1 = count2;
+        /*
+count1 = count2;
         count2 = milliseconds;
         milliseconds = 0;
         
@@ -34,8 +34,26 @@ if ($seconds >= 60) {
         console.log(count2);
         //add the two counts and divide by 2
         bpm = 60000/((count1 + count2)/2);
+*/
         
+        if (beats.length >= 10)
+            beats.shift();
+        var date = date = new Date();  
+        time = date.getTime();
         
+        beats.push({'time': time, 'frequency': time - beats[beats.length-1]['time']});
+        
+        var totalFrequency = 0;
+        $.each(beats, function() {
+            totalFrequency += this['frequency'];
+        });
+        //console.log(totalFrequency);
+        //console.log(beats.length);
+        //console.log(beats.length);
+        bpm = (60000*beats.length)/totalFrequency;
+        //console.log(totalFrequency);
+        //console.log(bpm);
+        $output.text(Math.round(bpm));
         
     });
 });
